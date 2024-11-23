ebb18e662a1dd40e77a6a8329a8ea7cb3f587994
dwiecko <wiecko.dawid@gmail.com>
2024-11-23 11:23:08 +0100

    add recipe-list component

--- recipe-manager/src/app/app.component.ts
+++ recipe-manager/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [RecipeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
