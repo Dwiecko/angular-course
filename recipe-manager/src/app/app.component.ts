607b79cd825cf44c75639d251b00483a9dd0d09b
dwiecko <wiecko.dawid@gmail.com>
2024-11-23 11:25:22 +0100

    Remove unused component

--- recipe-manager/src/app/app.component.ts
+++ recipe-manager/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @Component({
