a5b0e3a4a50c29edd378a4a9793e16f4a3a85d10
dwiecko <wiecko.dawid@gmail.com>
2024-11-23 14:38:51 +0100

    Add optional properties

--- recipe-manager/src/app/core/recipe/model/recipe.model.ts
+++ recipe-manager/src/app/core/recipe/model/recipe.model.ts
@@ -1,4 +1,8 @@
 export interface RecipeModel {
-    title: string;
-    description: string;
+       id?: number;                              // Unikalny identyfikator przepisu   
+       title: string;                           // Tytuł przepisu   
+       description: string;                     // Krótki opis przepisu   
+       ingredients?: string[];                   // Tablica składników   
+       preparationTime?: number;                 // Czas przygotowania w minutach   
+       difficulty?: 'easy' | 'medium' | 'hard';  // Poziom trudności 
 }
