d99a0c42f1b01a63fe60c240817c15dfb0693da4
dwiecko <wiecko.dawid@gmail.com>
2024-11-24 13:40:59 +0100

    Add description

--- recipe-manager/src/app/ui/recipe-reactive-form/recipe-reactive-form.component.ts
+++ recipe-manager/src/app/ui/recipe-reactive-form/recipe-reactive-form.component.ts
@@ -30,7 +30,7 @@ export class RecipeReactiveFormComponent implements OnInit {
   ngOnInit(): void {
     this.recipeFormGroup = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(3)]],
-      description: [''],
+      description: ['', [Validators.required]],
       ingredients: ['', Validators.required],
       preparationTime: ['', Validators.required],
       difficulty: ['easy', Validators.required]
@@ -55,7 +55,8 @@ export class RecipeReactiveFormComponent implements OnInit {
         ...this.recipeFormGroup.value, // ta linijka tworzy 'shadow copy' obecnych wartości formularza i je tu wrzuca, dzięki temu nie musimy ich deklarować ręcznie jeżeli się nie zmianiają, nie są mapowane, parsowane itp.
         ingredients: this.recipeFormGroup.value.ingredients.split(','), // metoda pomocnicza split(',') znajduje w ciągu znaków ',' i na tej podstawie rozdziela ciąg na części
         preparationTime: this.recipeFormGroup.value.preparationTime,
-        difficulty: this.recipeFormGroup.value.difficulty
+        difficulty: this.recipeFormGroup.value.difficulty,
+        description: this.recipeFormGroup.value.description
       };
 
       if (this.isEditMode) {
